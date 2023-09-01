dfd89948d0596c639ed8a27945d1768fdb4072cc
Noushadozi <noushadozi333@gmail.com>
2023-09-01 10:33:27 +0600

    data in card loaded

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const handleCategory = async () => {
     "https://openapi.programming-hero.com/api/news/categories"
   );
   const data = await response.json();
-  data.data.news_category.slice(0, 3).forEach((category) => {
+  data.data.news_category.slice(0, 5).forEach((category) => {
     const div = document.createElement("div");
     div.innerHTML = `
     <a onclick="handleLoadNews(${category.category_id})" class="tab">${category.category_name}</a>
@@ -14,7 +14,8 @@ const handleCategory = async () => {
 };
 
 const handleLoadNews = async (category) => {
-    const cardContainer = document.getElementById('card-container');
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
   const response = await fetch(
     `https://openapi.programming-hero.com/api/news/category/0${category}`
   );
@@ -23,18 +24,20 @@ const handleLoadNews = async (category) => {
     const div = document.createElement("div");
     div.innerHTML = `
     <div class="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+    <figure><img src=${news?.image_url} alt="Shoes" /></figure>
+    <div class="card-body">
+    <h2 class="card-title">${news.title}
+        <div class="badge badge-secondary">${news?.rating?.badge}</div>
+      </h2>
+      <p>${news.details.slice(0, 100)}...</p>
+      <div class="card-actions justify-end">
+        <div class="badge badge-outline">Fashion</div> 
+      </div>
     </div>
-  </div>
-</div>
-    `;
+  </div>`;
     cardContainer.appendChild(div);
   });
 };
 
 handleCategory();
+handleLoadNews(8);
